93f1e48e0327541ebff3577f1fb68b2bb2767e2b
J-dog-23 <124223697+J-dog-23@users.noreply.github.com>
2024-01-14 16:02:51 -0500

    Started to add door rotations

---
+++ Assets/CubeLeft.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       string x = "Jackson"; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        // }
+        string y = "Rob";
+
+    }
+}
